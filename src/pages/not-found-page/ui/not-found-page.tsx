644a365a065a83f0cd24b0a7731860f69d44e48c
zorin-mv <zorin.maksym@gmail.com>
2023-02-09 21:07:22 +0400

    feature/routing

---
+++ src/pages/not-found-page/ui/not-found-page.tsx
@@ -0,0 +1,18 @@
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/class-names';
+
+import classes from './not-found-page.module.scss';
+
+interface INotFoundPageProps {
+  className?: string;
+}
+
+export const NotFoundPage = ({ className }: INotFoundPageProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <div className={classNames(classes.notFoundPage, [className])}>
+      {t('not found')}
+    </div>
+  );
+};
